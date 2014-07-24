13cf6ff8e5d05c1e1145b4304a8bfb3f71b74874
root <root@carlosmalucelli.com>
2014-07-24 08:20:02 -0400

    ident fix

--- lib/vsapi.py
+++ lib/vsapi.py
@@ -1,156 +1,166 @@
-import urllib2
-from urllib2 import Request, urlopen, URLError
+#
+# VAG - Varnish Administration GUI
+#
 import re
+import urllib2
 import ConfigParser
+from urllib2 import Request, urlopen, URLError
 
 config = ConfigParser.RawConfigParser()
 config.read('config.cfg')
 
 class vsApi():
 		
-		def vcl_status(self,ipAgent):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/status/')
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				rtn = urllib2.urlopen(req,timeout = 2)
-				return rtn.read()
-			except URLError, e:
-				return e.reason
+	# Return varnish status
+	def vcl_status(self,ipAgent):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/status/')
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
 
-		def lastVCL(self,ipAgent):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://172.16.253.40:6085/vcl/')
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				res = []
-				res = urllib2.urlopen(req,timeout = 2).read().splitlines()
-				return res[-1].split()[2]
-			except URLError, e:
-				return e.read()
+	# Return last VCL added
+	def lastVCL(self,ipAgent):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://172.16.253.40:6085/vcl/')
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			res = []
+			res = urllib2.urlopen(req,timeout = 2).read().splitlines()
+			return res[-1].split()[2]
+		except URLError, e:
+			return e.read()
 
-		def vcl_active(self,ipAgent):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/vclactive/')
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				rtn = urllib2.urlopen(req,timeout = 2)
-				return rtn.read()
-			except URLError, e:
-				return e.reason
+	# Return VCL activated
+	def vcl_active(self,ipAgent):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/vclactive/')
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
 
-		def vcl_show(self,ipAgent,vclName):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/vcl/'+vclName)
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				res = urllib2.urlopen(req,timeout = 2)
-				conteudo = res.read()
-				aux = ""
-				for line in conteudo.splitlines():
-					if line.strip():
-						if re.match("^}",line):
-							aux = aux+line+'\n\n'
-						else:
-							aux = aux+line+'\n'
-				conteudo = aux
-				return conteudo
-			except URLError, e:
-				return e.read()
+	# Return content of VCL
+	def vcl_show(self,ipAgent,vclName):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/vcl/'+vclName)
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			res = urllib2.urlopen(req,timeout = 2)
+			conteudo = res.read()
+			aux = ""
+			for line in conteudo.splitlines():
+				if line.strip():
+					if re.match("^}",line):
+						aux = aux+line+'\n\n'
+					else:
+						aux = aux+line+'\n'
+			conteudo = aux
+			return conteudo
+		except URLError, e:
+			return e.read()
 
-		def vcl_save(self,ipAgent,vclContent):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/vcl/', vclContent)
-			req.get_method = lambda: 'POST'
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				res = urllib2.urlopen(req,timeout = 2)
-				# Use VCL
-				vclName = self.lastVCL(ipAgent)
-				rtn = res.read()+" "+self.vcl_use(ipAgent,vclName)
-				return rtn
-			except URLError, e:
-				return e.reason
+	# Save VCL
+	def vcl_save(self,ipAgent,vclContent):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/vcl/', vclContent)
+		req.get_method = lambda: 'POST'
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			res = urllib2.urlopen(req,timeout = 2)
+			vclName = self.lastVCL(ipAgent)
+			rtn = res.read()+" "+self.vcl_use(ipAgent,vclName)
+			return rtn
+		except URLError, e:
+			return e.read()
 
-		def vcl_use(self,ipAgent,vclName):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/vcldeploy/'+vclName, data=vclName)
-			req.add_header('Content-Type', 'text/plain')
-			req.get_method = lambda: 'PUT'
-			req.add_header('Authorization', userData)
-			try:
-				rtn = urllib2.urlopen(req,timeout = 2)
-				if rtn.code == 200:
-					rtn = "VCL "+vclName+" activated!"
-				else:
-					rtn = "VCL use error: "+rtn.msg
-				return rtn
-			except URLError, e:
-				return e.read()
+	# Active VCL
+	def vcl_use(self,ipAgent,vclName):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/vcldeploy/'+vclName, data=vclName)
+		req.add_header('Content-Type', 'text/plain')
+		req.get_method = lambda: 'PUT'
+		req.add_header('Authorization', userData)
+		try:
+			rtn = urllib2.urlopen(req,timeout = 2)
+			if rtn.code == 200:
+				rtn = "VCL "+vclName+" activated!"
+			else:
+				rtn = "VCL use error: "+rtn.msg
+			return rtn
+		except URLError, e:
+			return e.read()
 
-		def vcl_ban(self,ipAgent,domain,uri):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/ban/'+uri)
-			req.get_method = lambda: 'POST'
-			req.add_header('Accept', 'application/json')
-			req.add_header('Host', domain)
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				rtn = urllib2.urlopen(req,timeout = 2)
-				if rtn.code == 200:
-					rtn = "http://"+domain+"/"+uri+" banned!"
-				else:
-					rtn = "BAN error: http://"+domain+"/"+uri
-				return rtn
-			except URLError, e:
-				return str(e.reason)
+	# Ban/Purge
+	def vcl_ban(self,ipAgent,domain,uri):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/ban/'+uri)
+		req.get_method = lambda: 'POST'
+		req.add_header('Accept', 'application/json')
+		req.add_header('Host', domain)
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			rtn = urllib2.urlopen(req,timeout = 2)
+			if rtn.code == 200:
+				rtn = "http://"+domain+"/"+uri+" banned!"
+			else:
+				rtn = "BAN error: http://"+domain+"/"+uri
+			return rtn
+		except URLError, e:
+			return str(e.reason)
 
-		def vcl_ban_list(self,ipAgent):
-			uName = config.get('conf','vaName')
-			pWord = config.get('conf','vaPass')
-			userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-			req = urllib2.Request('http://'+ipAgent+':6085/ban/')
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			try:
-				rtn = urllib2.urlopen(req,timeout = 1)
-				list = []
-				count = 0
-				for line in rtn.read().splitlines():
-					if not (line == "Present bans:"):
-						# 5 last bans by cluter
-						if count < 5:
-							list.append(line.split()[2]+" "+line.split()[3]+" "+line.split()[4])
-							count = count+1
-						else:
-							count = 0
-							break
-				return list
-			except URLError, e:
-				return e.reason
+	# List last 5 bans 
+	def vcl_ban_list(self,ipAgent):
+		uName = config.get('conf','vaName')
+		pWord = config.get('conf','vaPass')
+		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+		req = urllib2.Request('http://'+ipAgent+':6085/ban/')
+		req.add_header('Accept', 'application/json')
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header('Authorization', userData)
+		try:
+			rtn = urllib2.urlopen(req,timeout = 1)
+			list = []
+			count = 0
+			for line in rtn.read().splitlines():
+				if not (line == "Present bans:"):
+					# 5 last bans by cluter
+					if count < 5:
+						list.append(line.split()[2]+" "+line.split()[3]+" "+line.split()[4])
+						count = count+1
+					else:
+						count = 0
+						break
+			return list
+		except URLError, e:
+			return e.reason
