de1117e060804b36d44f8c332872b71f1faa51eb
root <root@carlosmalucelli.com>
2014-08-04 21:47:34 -0400

    code refactor

--- lib/vsapi.py
+++ lib/vsapi.py
@@ -12,16 +12,48 @@ config.read('config.cfg')
 
 class vsApi():
 		
-	# Return varnish status
-	def vcl_status(self,ipAgent):
+	#def urlibInfo(self):
+	#	uName = config.get('conf','vaName')
+	#	pWord = config.get('conf','vaPass')
+	#	userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
+	#	return userData
+
+	def urlRequest(self,ipAgent,action,extra=None,mtd=None,domain=None):
 		uName = config.get('conf','vaName')
 		pWord = config.get('conf','vaPass')
 		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/status/')
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
+		if (extra != None):
+			try:
+				if mtd == "POST":
+					req = urllib2.Request('http://'+ipAgent+':6085/'+action+'/',str(extra))
+					req.get_method = lambda: 'POST'
+				elif mtd == "PUT":
+					req = urllib2.Request('http://'+ipAgent+':6085/'+action+'/'+str(extra), data=str(extra))
+					req.get_method = lambda: 'PUT'
+				else:
+					req = urllib2.Request('http://'+ipAgent+':6085/'+action+'/'+str(extra))
+				if domain != None:
+					req.add_header('Host', domain)
+				req.add_header('Accept', 'application/json')
+				req.add_header("Content-type", "application/x-www-form-urlencoded")
+				req.add_header('Authorization', userData)
+				return req
+			except URLError, e:
+				return e.reason
+		else:
+			try:
+				req = urllib2.Request('http://'+str(ipAgent)+':6085/'+str(action)+'/')
+				req.add_header('Accept', 'application/json')
+				req.add_header("Content-type", "application/x-www-form-urlencoded")
+				req.add_header('Authorization', userData)
+				return req
+			except URLError, e:
+				return e.reason
+
+	# Return varnish status
+	def vcl_status(self,ipAgent):
 		try:
+			req = self.urlRequest(ipAgent,"status")
 			rtn = urllib2.urlopen(req,timeout = 2)
 			return rtn.read()
 		except URLError, e:
@@ -29,14 +61,8 @@ class vsApi():
 
 	# Return last VCL added
 	def lastVCL(self,ipAgent):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/vcl/')
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"vcl")
 			res = []
 			res = urllib2.urlopen(req,timeout = 2).read().splitlines()
 			return res[-1].split()[2]
@@ -45,14 +71,8 @@ class vsApi():
 
 	# Return VCL activated
 	def vcl_active(self,ipAgent):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/vclactive/')
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"vclactive")
 			rtn = urllib2.urlopen(req,timeout = 2)
 			return rtn.read()
 		except URLError, e:
@@ -60,14 +80,8 @@ class vsApi():
 
 	# Return content of VCL
 	def vcl_show(self,ipAgent,vclName):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/vcl/'+vclName)
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"vcl",vclName)
 			res = urllib2.urlopen(req,timeout = 2)
 			conteudo = res.read()
 			aux = ""
@@ -80,36 +94,23 @@ class vsApi():
 			conteudo = aux
 			return conteudo
 		except URLError, e:
-			return e.read()
+			return e.reason
 
 	# Save VCL
 	def vcl_save(self,ipAgent,vclContent):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/vcl/', vclContent)
-		req.get_method = lambda: 'POST'
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"vcl",vclContent,"POST")
 			res = urllib2.urlopen(req,timeout = 2)
 			vclName = self.lastVCL(ipAgent)
 			rtn = res.read()+" "+self.vcl_use(ipAgent,vclName)
 			return rtn
 		except URLError, e:
-			return e.read()
+			return e.reason
 
 	# Active VCL
 	def vcl_use(self,ipAgent,vclName):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/vcldeploy/'+vclName, data=vclName)
-		req.add_header('Content-Type', 'text/plain')
-		req.get_method = lambda: 'PUT'
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"vcldeploy",vclName,"PUT")
 			rtn = urllib2.urlopen(req,timeout = 2)
 			if rtn.code == 200:
 				rtn = "VCL "+vclName+" activated!"
@@ -117,20 +118,12 @@ class vsApi():
 				rtn = "VCL use error: "+rtn.msg
 			return rtn
 		except URLError, e:
-			return e.read()
+			return e.reason
 
 	# Ban/Purge
 	def vcl_ban(self,ipAgent,domain,uri):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/ban/'+uri)
-		req.get_method = lambda: 'POST'
-		req.add_header('Accept', 'application/json')
-		req.add_header('Host', domain)
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"ban",uri,"POST",domain)
 			rtn = urllib2.urlopen(req,timeout = 2)
 			if rtn.code == 200:
 				rtn = "http://"+domain+"/"+uri+" banned!"
@@ -142,14 +135,8 @@ class vsApi():
 
 	# List last 5 bans 
 	def vcl_ban_list(self,ipAgent):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/ban/')
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"ban")
 			rtn = urllib2.urlopen(req,timeout = 1)
 			list = []
 			count = 0
@@ -161,6 +148,7 @@ class vsApi():
 						count = count+1
 					else:
 						count = 0
+						# Sorry about that :(
 						break
 			return list
 		except URLError, e:
@@ -168,14 +156,8 @@ class vsApi():
 
 	# Return varnish stats
 	def varnish_stats(self,ipAgent):
-		uName = config.get('conf','vaName')
-		pWord = config.get('conf','vaPass')
-		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
-		req = urllib2.Request('http://'+ipAgent+':6085/stats')
-		req.add_header('Accept', 'application/json')
-		req.add_header("Content-type", "application/x-www-form-urlencoded")
-		req.add_header('Authorization', userData)
 		try:
+			req = self.urlRequest(ipAgent,"stats")
 			rtn = urllib2.urlopen(req,timeout = 2)
 			return rtn.read()
 		except URLError, e:
