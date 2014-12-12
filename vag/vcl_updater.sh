ceec93b06bb6b56f7dbbb32cf119d2cc364dc2fa
Carlos Augusto Malucelli <carlos@carlosmalucelli.com>
2014-12-12 08:26:11 -0200

    Create vcl_updater.sh

---
+++ vag/vcl_updater.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+BACKUPDIR="/etc/varnish/backup/"
+AGENTDIR="/var/lib/varnish-agent/"
+VCLDEFAULT="/etc/varnish/default.vcl"
+LOGFILE="/abd/scripts/vcl.log"
+AGENTVCL=$(find $AGENTDIR -mtime -1 -type f \( -iname \*.vcl ! -iname boot.vcl \) -exec ls -t1 {} \; | sed '1!d' )
+
+# backup default vcl
+cp $VCLDEFAULT $BACKUPDIR/$(date +%Y%m%d%H%M).vcl
+
+if [ $? -eq 0 ]; then
+	echo "Backup OK - $(date +%Y/%m/%d-%H:%M:%S)" | tee -a $LOGFILE
+else
+	echo "Backup ERROR - $(date +%Y/%m/%d-%H:%M:%S)" | tee -a $LOGFILE
+	exit 2
+fi
+
+# find new file and copy to default vcl
+cp $AGENTVCL $VCLDEFAULT &>/dev/null
+
+if [ $? -eq 0 ]; then
+	echo "Copy OK - $(date +%Y/%m/%d-%H:%M:%S)" | tee -a $LOGFILE
+else
+	echo "Copy ERROR - $(date +%Y/%m/%d-%H:%M:%S)" | tee -a $LOGFILE
+	exit 2
+fi
+
+# remove old configs
+find $BACKUPDIR -cmin +640 -exec rm -fv {} \;
+
+exit 0
